61820c26f281cb74dbf0c89b1da25b54f797d5c5
Andrew Eubanks <aeubanksguitar@gmail.com>
2023-06-04 10:12:37 -0400

    filled out app

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+const path = require('path');
 const express = require('express');
-const port = 5000;
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT;
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
+
+//Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//cors middleware
+app.use(
+  cors({
+    origin: ['http://localhost:5000', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 
 app.get('/', (req, res) => {
   res.send({ message: 'Welcome to the RandomIdeas API' });
